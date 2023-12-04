c1d0906aeb260298481a39df410c0790e836b890
francoperez1 <francoantonioperezvargas8@gmail.com>
2023-12-04 15:22:52 +0000

    Haciendo commit de listado.py para empezar a hacer cambios

---
+++ listado.py
@@ -0,0 +1,9 @@
+# Lista de nombres y apellidos
+nombres_apellidos = [
+    "Franco Perez",
+    "Benjamin Guajardo"
+]
+
+# Imprimir el listado
+for nombre_apellido in nombres_apellidos:
+    print(nombre_apellido)
